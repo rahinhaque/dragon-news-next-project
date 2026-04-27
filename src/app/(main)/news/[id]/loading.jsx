867d4f1480e8c2fe809200b6aeb3bd477d6bcc7f
Rahin Haque <haquerahin743@gmail.com>
2026-04-27 22:07:16 +0600

    Added loading to Category and and news details

--- src/app/(main)/news/[id]/loading.jsx
+++ src/app/(main)/news/[id]/loading.jsx
@@ -6,17 +6,17 @@ const loading = () => {
       <div className="relative flex items-center justify-center">
         {/* Outer pulse effect */}
         <div className="absolute w-24 h-24 bg-red-500/10 dark:bg-red-500/5 rounded-full animate-ping"></div>
-        
+
         {/* DaisyUI Loading component */}
         <span className="loading loading-infinity loading-lg text-red-600 dark:text-red-500 scale-150"></span>
       </div>
-      
+
       <div className="flex flex-col items-center animate-pulse">
         <h3 className="text-xl font-bold text-slate-800 dark:text-slate-100 tracking-wider">
           DRAGON NEWS
         </h3>
         <p className="text-sm text-slate-500 dark:text-slate-400 font-medium">
-           New stories everyday...
+          Fetching News details...
         </p>
       </div>
     </div>
