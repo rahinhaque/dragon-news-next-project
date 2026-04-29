37aa2305b88c700b8206c8e6e7a35e0debe9b9db
Rahin Haque <haquerahin743@gmail.com>
2026-04-29 22:12:58 +0600

    implemented proxy or middleware for about and career page

---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+export async function proxy(request) {
+  // return NextResponse.redirect(new URL("/login", request.url));
+
+ const session = await auth.api.getSession({
+   headers: await headers(),
+ });
+ if (!session) {
+   return NextResponse.redirect(new URL("/login", request.url));
+ } else {
+   return NextResponse.next();
+ }
+  
+}
+ 
+
+export const config = {
+  matcher: ["/about" , "/career" ],
+};
